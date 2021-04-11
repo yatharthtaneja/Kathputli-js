481a594479b55d99ead2432bd10143c1c4df1fa1
Yatharth Taneja <yatharth19346@iiitd.ac.in>
2021-04-11 16:00:44 +0530

    added models and rotation

---
+++ js/media.js
@@ -0,0 +1,211 @@
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+function onResults(results) {
+// Hide the spinner.
+document.body.classList.add('loaded');
+
+
+// Update the frame rate.
+//   fpsControl.tick();
+
+// Draw the overlays.
+canvasCtx.save();
+canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+canvasCtx.drawImage(
+  results.image, 0, 0, canvasElement.width, canvasElement.height);
+if (results.multiHandLandmarks && results.multiHandedness) {
+for (let index = 0; index < results.multiHandLandmarks.length; index++) {
+  const classification = results.multiHandedness[index];
+  const isRightHand = classification.label === 'Right';
+  const landmarks = results.multiHandLandmarks[index];
+  drawConnectors(
+      canvasCtx, landmarks, HAND_CONNECTIONS,
+      {color: isRightHand ? '#00FF00' : '#FF0000'}),
+  drawLandmarks(canvasCtx, landmarks, {
+    color: isRightHand ? '#00FF00' : '#FF0000',
+    fillColor: isRightHand ? '#FF0000' : '#00FF00',
+    radius: (x) => {
+      return lerp(x.from.z, -0.15, .1, 10, 1);
+    }
+  });
+}
+
+// console.log(results.multiHandedness);
+if(results.multiHandLandmarks.length == 1){
+
+if(results.multiHandedness[0].index==0){ //for left
+        globalVariable.x1 = results.multiHandLandmarks[0][0].x * 1280;
+        globalVariable.y1 = results.multiHandLandmarks[0][0].y *720;
+        globalVariable.z1 = Math.abs(results.multiHandLandmarks[0][5].x - results.multiHandLandmarks[0][17].x) *1280;
+        globalVariable.dist1 = Math.abs(results.multiHandLandmarks[0][8].x - results.multiHandLandmarks[0][4].x) *1280;
+
+        globalVariable.r8x =  results.multiHandLandmarks[0][8].x *1280
+        globalVariable.r8y =  results.multiHandLandmarks[0][8].y *720
+        globalVariable.r6x =  results.multiHandLandmarks[0][6].x*1280
+        globalVariable.r6y =  results.multiHandLandmarks[0][6].y *720
+        globalVariable.r16x =  results.multiHandLandmarks[0][16].x*1280
+        globalVariable.r16y =  results.multiHandLandmarks[0][16].y *720
+        globalVariable.r9x =  results.multiHandLandmarks[0][9].x*1280
+        globalVariable.r9y =  results.multiHandLandmarks[0][9].y *720
+
+        globalVariable.r12x =  results.multiHandLandmarks[0][12].x*1280
+        globalVariable.r12y =  results.multiHandLandmarks[0][12].y *720
+        globalVariable.r20x =  results.multiHandLandmarks[0][20].x*1280
+        globalVariable.r20y =  results.multiHandLandmarks[0][20].y *720
+
+        globalVariable.dist1y = globalVariable.y1 - globalVariable.r9y 
+        globalVariable.rotate1 = results.multiHandLandmarks[0][8].z
+        // console.log(globalVariable.rotate1)
+        globalVariable.x2 = -1000;
+        globalVariable.y2 = -1000; 
+}
+else if(results.multiHandedness[0].index==1){ //for right
+        globalVariable.x2 = results.multiHandLandmarks[0][0].x * 1280;
+        globalVariable.y2 = results.multiHandLandmarks[0][0].y *720;
+        globalVariable.z2 = Math.abs(results.multiHandLandmarks[0][5].x - results.multiHandLandmarks[0][17].x) *1280;
+        globalVariable.dist2 = Math.abs(results.multiHandLandmarks[0][8].x - results.multiHandLandmarks[0][4].x) *1280;
+
+
+        globalVariable.l8x =  results.multiHandLandmarks[0][8].x *1280
+        globalVariable.l8y =  results.multiHandLandmarks[0][8].y *720
+        
+        globalVariable.l6x =  results.multiHandLandmarks[0][6].x*1280
+        globalVariable.l6y =  results.multiHandLandmarks[0][6].y *720
+        globalVariable.l16x =  results.multiHandLandmarks[0][16].x*1280
+        globalVariable.l16y =  results.multiHandLandmarks[0][16].y *720
+        globalVariable.l9x =  results.multiHandLandmarks[0][9].x*1280
+        globalVariable.l9y =  results.multiHandLandmarks[0][9].y *720
+
+        globalVariable.l12x =  results.multiHandLandmarks[0][12].x*1280
+        globalVariable.l12y =  results.multiHandLandmarks[0][12].y *720
+        globalVariable.l20x =  results.multiHandLandmarks[0][20].x*1280
+        globalVariable.l20y =  results.multiHandLandmarks[0][20].y *720
+
+        globalVariable.x1 = -1000;
+        globalVariable.y1 = -1000; 
+
+        globalVariable.dist2y = globalVariable.y2 - globalVariable.l9y 
+
+}
+
+
+}
+else if(results.multiHandLandmarks.length == 2){
+
+    if(results.multiHandedness[0].index==0){ //for left
+            globalVariable.x1 = results.multiHandLandmarks[0][0].x * 1280;
+            globalVariable.y1 = results.multiHandLandmarks[0][0].y *720;
+            globalVariable.z1 = Math.abs(results.multiHandLandmarks[0][5].x - results.multiHandLandmarks[0][17].x) *1280;
+            globalVariable.dist1 = Math.abs(results.multiHandLandmarks[0][8].x - results.multiHandLandmarks[0][4].x) *1280;
+
+            globalVariable.r8x =  results.multiHandLandmarks[0][8].x *1280
+            globalVariable.r8y =  results.multiHandLandmarks[0][8].y *720
+            globalVariable.r6x =  results.multiHandLandmarks[0][6].x*1280
+            globalVariable.r6y =  results.multiHandLandmarks[0][6].y *720
+            globalVariable.r16x =  results.multiHandLandmarks[0][16].x*1280
+            globalVariable.r16y =  results.multiHandLandmarks[0][16].y *720
+            globalVariable.r9x =  results.multiHandLandmarks[0][9].x*1280
+            globalVariable.r9y =  results.multiHandLandmarks[0][9].y *720
+
+            globalVariable.r12x =  results.multiHandLandmarks[0][12].x*1280
+            globalVariable.r12y =  results.multiHandLandmarks[0][12].y *720
+            globalVariable.r20x =  results.multiHandLandmarks[0][20].x*1280
+            globalVariable.r20y =  results.multiHandLandmarks[0][20].y *720
+
+            globalVariable.dist1y = globalVariable.y1 - globalVariable.r9y 
+
+    }
+    else if(results.multiHandedness[0].index==1){ //for right
+            globalVariable.x2 = results.multiHandLandmarks[0][0].x * 1280;
+            globalVariable.y2 = results.multiHandLandmarks[0][0].y *720;
+            globalVariable.z2 = Math.abs(results.multiHandLandmarks[0][5].x - results.multiHandLandmarks[0][17].x) *1280;
+            globalVariable.dist2 = Math.abs(results.multiHandLandmarks[0][8].x - results.multiHandLandmarks[0][4].x) *1280;
+
+            globalVariable.l8x =  results.multiHandLandmarks[0][8].x *1280
+            globalVariable.l8y =  results.multiHandLandmarks[0][8].y *720
+            globalVariable.l6x =  results.multiHandLandmarks[0][6].x*1280
+            globalVariable.l6y =  results.multiHandLandmarks[0][6].y *720
+            globalVariable.l16x =  results.multiHandLandmarks[0][16].x*1280
+            globalVariable.l16y =  results.multiHandLandmarks[0][16].y *720
+            globalVariable.l9x =  results.multiHandLandmarks[0][9].x*1280
+            globalVariable.l9y =  results.multiHandLandmarks[0][9].y *720
+
+            globalVariable.dist2y = globalVariable.y2 - globalVariable.l9y 
+
+            globalVariable.l12x =  results.multiHandLandmarks[0][12].x*1280
+            globalVariable.l12y =  results.multiHandLandmarks[0][12].y *720
+            globalVariable.l20x =  results.multiHandLandmarks[0][20].x*1280
+            globalVariable.l20y =  results.multiHandLandmarks[0][20].y *720
+    
+    }
+    
+    if(results.multiHandedness[1].index==0){ //for left
+        globalVariable.x1 = results.multiHandLandmarks[1][0].x * 1280;
+        globalVariable.y1 = results.multiHandLandmarks[1][0].y *720;
+        globalVariable.z1 = Math.abs(results.multiHandLandmarks[1][5].x - results.multiHandLandmarks[1][17].x) *1280;
+        globalVariable.dist1 = Math.abs(results.multiHandLandmarks[1][8].x - results.multiHandLandmarks[1][4].x) *1280;
+        globalVariable.r8x =  results.multiHandLandmarks[1][8].x *1280
+        globalVariable.r8y =  results.multiHandLandmarks[1][8].y *720
+        globalVariable.r6x =  results.multiHandLandmarks[1][6].x*1280
+        globalVariable.r6y =  results.multiHandLandmarks[1][6].y *720
+        globalVariable.r16x =  results.multiHandLandmarks[1][16].x*1280
+        globalVariable.r16y =  results.multiHandLandmarks[1][16].y *720
+        globalVariable.r17x =  results.multiHandLandmarks[1][17].x*1280
+        globalVariable.r17y =  results.multiHandLandmarks[1][17].y *720
+        globalVariable.dist1y = globalVariable.y1 - globalVariable.r9y 
+
+        globalVariable.r12x =  results.multiHandLandmarks[1][12].x*1280
+        globalVariable.r12y =  results.multiHandLandmarks[1][12].y *720
+        globalVariable.r20x =  results.multiHandLandmarks[1][20].x*1280
+        globalVariable.r20y =  results.multiHandLandmarks[1][20].y *720
+}
+    else if(results.multiHandedness[1].index==1){ //for right
+        globalVariable.x2 = results.multiHandLandmarks[1][0].x * 1280;
+        globalVariable.y2 = results.multiHandLandmarks[1][0].y *720;
+        globalVariable.z2 = Math.abs(results.multiHandLandmarks[1][5].x - results.multiHandLandmarks[1][17].x) *1280;
+        globalVariable.dist2 = Math.abs(results.multiHandLandmarks[1][8].x - results.multiHandLandmarks[1][4].x) *1280;
+
+        globalVariable.l8x =  results.multiHandLandmarks[1][8].x *1280
+        globalVariable.l8y =  results.multiHandLandmarks[1][8].y *720
+        globalVariable.l6x =  results.multiHandLandmarks[1][6].x*1280
+        globalVariable.l6y =  results.multiHandLandmarks[1][6].y *720
+        globalVariable.l16x =  results.multiHandLandmarks[1][16].x*1280
+        globalVariable.l16y =  results.multiHandLandmarks[1][16].y *720
+        globalVariable.l17x =  results.multiHandLandmarks[1][17].x*1280
+        globalVariable.l17y =  results.multiHandLandmarks[1][17].y *720
+        globalVariable.dist2y = globalVariable.y2 - globalVariable.l9y 
+
+        globalVariable.l12x =  results.multiHandLandmarks[1][12].x*1280
+        globalVariable.l12y =  results.multiHandLandmarks[1][12].y *720
+        globalVariable.l20x =  results.multiHandLandmarks[1][20].x*1280
+        globalVariable.l20y =  results.multiHandLandmarks[1][20].y *720
+}
+    
+    }
+}
+else{
+    globalVariable.x1 = -1000;
+    globalVariable.y1 = -1000; 
+    globalVariable.x2 = -1000;
+    globalVariable.y2 = -1000; 
+}
+canvasCtx.restore();
+}
+
+const hands = new Hands({locateFile: (file) => {
+return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.1/${file}`;
+}});
+hands.onResults(onResults);
+/**
+* Instantiate a camera. We'll feed each frame we receive into the solution.
+*/
+const camera = new Camera(videoElement, {
+onFrame: async () => {
+await hands.send({image: videoElement});
+},
+width: 1280,
+height: 720
+});
+camera.start();
---
+++ js/script.js
@@ -0,0 +1,282 @@
+// (function() {
+    // Set our main variables
+    let scene,  
+      renderer,
+      container,
+      camera,
+      model,                              // Our character
+      neck,                               // Reference to the neck bone in the skeleton
+      waist,  
+      Lforearm,
+      Rforearm,                             // Reference to the waist bone in the skeleton
+      possibleAnims,                      // Animations found in our file
+      mixer,                              // THREE.js animations mixer
+      idle,   
+      cheer,                            // Idle, the default state our character returns to
+      clock = new THREE.Clock(),          // Used for anims, which run to a clock instead of frame rate 
+      currentlyAnimating = false,         // Used to check whether characters neck is being used in another anim
+      raycaster = new THREE.Raycaster(),  // Used to detect the click on our character
+      loaderAnim = document.getElementById('js-loader');
+    
+    // })();
+
+    
+
+    init(); 
+
+    function init() {
+    
+        const MODEL_PATH ='img/remote.glb';
+        
+        // Init the scene
+        scene = new THREE.Scene();
+
+
+        //Init Renderer
+        renderer = new THREE.WebGLRenderer({ alpha : true});
+        renderer.shadowMap.enabled = true;
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(1066, 600);
+        renderer.setClearColor( 0x000000, 0 );
+        container = document.getElementById('wrapper').appendChild(renderer.domElement);
+
+        // document.body.appendChild(renderer.domElement);
+
+        // Add a camera
+        camera = new THREE.PerspectiveCamera(
+            50,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000
+        );
+        camera.position.z = 30 
+        camera.position.x = 0;
+        camera.position.y = -3;
+
+
+        var loader = new THREE.GLTFLoader();
+
+        loader.load(
+        MODEL_PATH,
+        function(gltf) {
+        // A lot is going to happen here
+        model = gltf.scene;
+        let fileAnimations = gltf.animations;
+        // console.log(fileAnimations);
+
+        model.traverse(o => {
+            
+            if (o.isBone) {
+              console.log(o.name);
+            }
+
+            if (o.isMesh) {
+              o.castShadow = true;
+              o.receiveShadow = true;
+              // o.material = stacy_mtl;
+            }
+          
+            // Reference the neck and waist bones
+          //   if (o.isBone && o.name === 'mixamorigLeftArm') { 
+          //       neck = o;
+          //   }
+          //   if (o.isBone && o.name === 'mixamorigRightArm') { 
+          //     // console.log("ess");
+          //       waist = o;
+          //   }
+
+          //   if (o.isBone && o.name === 'mixamorigLeftForeArm') { 
+          //     Lforearm = o;
+          // }
+          // if (o.isBone && o.name === 'mixamorigRightForeArm') { 
+          //   // console.log("ess");
+          //     Rforearm = o;
+          // }
+
+            if (o.isBone && o.name === 'mixamorigLeftShoulder') { 
+                neck = o;
+            }
+            if (o.isBone && o.name === 'mixamorigRightShoulder') { 
+              waist = o;
+          }
+
+        });
+
+          // Set the models initial scale
+        model.scale.set(120, 120, 120);
+
+        model.position.y = -11;
+    
+        scene.add(model);
+
+        // loaderAnim.remove();
+
+        mixer = new THREE.AnimationMixer(model);
+
+        let idleAnim = THREE.AnimationClip.findByName(fileAnimations, 'idle');
+        // console.log(idleAnim.tracks);
+        idleAnim.tracks.splice(21, 3);
+        idleAnim.tracks.splice(78, 3);
+        idle = mixer.clipAction(idleAnim);
+        idle.play();
+
+        },
+        undefined, // We don't need this function
+        function(error) {
+            console.error(error);
+        }
+        
+        );
+
+        // Add lights
+        let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
+        hemiLight.position.set(0, 50, 0);
+        // Add hemisphere light to scene
+        scene.add(hemiLight);
+
+        let d = 8.25;
+        let dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
+        dirLight.position.set(-8, 12, 8);
+        dirLight.castShadow = true;
+        dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+        dirLight.shadow.camera.near = 0.1;
+        dirLight.shadow.camera.far = 1500;
+        dirLight.shadow.camera.left = d * -1;
+        dirLight.shadow.camera.right = d;
+        dirLight.shadow.camera.top = d;
+        dirLight.shadow.camera.bottom = d * -1;
+        // Add directional Light to scene
+        scene.add(dirLight);
+    }
+
+    function update() {
+
+        if (mixer) {
+            mixer.update(clock.getDelta());
+          }
+        renderer.render(scene, camera);
+        requestAnimationFrame(update);
+
+        var tag = document.getElementById('wrapper');
+        tag.style.top = (globalVariable.y2+100) + 'px';
+        tag.style.left = globalVariable.x2 + 'px';
+        // console.log(waist)
+        if(neck && waist){
+          // moveJoint2(globalVariable.x1, globalVariable.y1 , neck);
+          checkJoint(globalVariable.y2- globalVariable.l16y, -1,neck);
+          checkJoint(globalVariable.y2- globalVariable.l8y , 1 ,waist);
+          // console.log(neck.rotation.x);
+
+
+        }
+
+        if(globalVariable.l12y >= globalVariable.l9y && globalVariable.l20y >= globalVariable.l9y ){
+          // console.log(model)
+          // model.rotation.y += 0.005;
+          model.rotation.y += 0.01
+      }
+        // if (!currentlyAnimating  && globalVariable.z2> 200) {
+        //   currentlyAnimating = true;
+        //   playOnClick();
+          // && globalVariable.z1> 200
+      // }
+
+      }
+      update();
+
+// deadpool
+var temp;
+var i = 0 , csize = 5 ;
+
+var arr =new Array(csize) ;
+
+function smooth(arr){
+  var sum = 0;
+  for(var j = 0 ; j < csize ; j++){
+    sum+= arr[j];
+  }
+  return sum / csize ;
+}
+      function checkJoint(y, sign, joint){
+          // const old_min = -350 , old_max = -75 , new_max = 2100 , new_min = -1.5;
+          // joint.rotation.x=( (y - globalVariable.dist2*2) / (globalVariable.dist2*2 -  0) ) * (1.7 - (-1.5)) + (-1.5)
+
+        temp =  ( (y - globalVariable.dist2*2) / (globalVariable.dist2*2 -  0) ) * (1.7 - (-1.5)) + (-1.5)
+        // temp =  ( (y -globalVariable.dist2y*2) / (globalVariable.dist2y*2 -  0) ) * (1.7 - (-1.7)) + (-1.7)
+        if (temp < -1.7)
+          temp = -1.7;
+        if (temp >1.7)
+        temp = 1.7;
+        // else if (temp < -1.5)
+
+        joint.rotation.x = -1* temp.toFixed(4);
+        // console.log(joint.name , globalVariable.dist2y*2 )
+
+        // arr[i++] = temp ;
+        // if (i== csize ){
+        //   i=0;
+        //   joint.rotation.x = smooth(arr);
+        //   arr =new Array(csize) ;
+        // }
+        // console.log( "x: ", joint.rotation.x)
+        joint.rotation.z = sign * 0.9;
+        joint.rotation.y = -1 * sign * 0.3;
+
+      }
+
+
+      function getMousePos(x1,y1) {
+        return { x: x1, y: y1 };
+      }
+
+      function getMousePoscoord(e) {
+        console.log(e.clientX, e.clientY );
+      }
+      
+      function moveJoint(x,y, joint, degreeLimit) {
+
+        let degrees = getMouseDegrees(x, y, degreeLimit);
+
+        joint.rotation.y = THREE.Math.degToRad(degrees.y);
+        joint.rotation.x = THREE.Math.degToRad(degrees.x);
+      }
+
+      function moveJoint2(x,y, joint) {
+
+
+        // joint.lookAt(x,y,100);
+        // joint.lookAt(x,y,100);
+
+        // const v = new THREE.Vector3(0,1,1);
+        // console.log((globalVariable.y2 - globalVariable.l8y )/ 720 * 360 );
+        // console.log(joint.rotation.x ,joint.rotation.z );
+        // joint.rotateY(THREE.Math.degToRad((globalVariable.y2 - globalVariable.l8y )/ 720 * 360 ));
+        joint.rotateY(THREE.Math.degToRad(Math.PI / 2) );
+        
+        // isko koi nahi chedhega
+        // joint.rotation.z = -1.7;
+        // joint.rotation.x = 1.7;
+        // joint.rotation.y = 0.3;
+
+        joint.rotation.z = -1.7;
+        joint.rotation.x = 1.8;
+        joint.rotation.y = 0.3;
+        
+      }
+
+    function playOnClick() {
+        let anim = Math.floor(Math.random() * possibleAnims.length) + 0;
+        playModifierAnimation(idle, 0.25, cheer, 0.25);
+      }
+
+      function playModifierAnimation(from, fSpeed, to, tSpeed) {
+        to.setLoop(THREE.LoopOnce);
+        to.reset();
+        to.play();
+        from.crossFadeTo(to, fSpeed, true);
+        setTimeout(function() {
+          from.enabled = true;
+          to.crossFadeTo(from, tSpeed, true);
+          currentlyAnimating = false;
+        }, to._clip.duration * 1000 - ((tSpeed + fSpeed) * 1000));
+      }
